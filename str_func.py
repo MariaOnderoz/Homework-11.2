95164980a1a25257d41b4ed50fc9418c5daef5b2
mariaonderoz <mskorneeva.87@gmail.com>
2023-05-21 00:10:05 +0300

    added str_func2 in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def str_func(value):
     """Функция возвращает заглавные буквы"""
-    return value.upper() 
+    return value.upper()
+
+
+def str_func2(value):
+    """Функция делает заглавными первые буквы каждого слова"""
+    return value.title() 
